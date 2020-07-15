2fe2562bb81ab9dd673eec4bcf34a0cd4f25e2b1
Gifary Dhimas Fadhillah <gifarydhimas@gmail.com>
2020-07-15 21:10:47 +0700

    Handle loading and error on ProductList page

--- web/src/pages/ProductList/index.tsx
+++ web/src/pages/ProductList/index.tsx
@@ -9,7 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, CircularProgress } from '@material-ui/core';
 
 import Product from '../../lib/model/Product';
 import { useParams } from 'react-router-dom';
@@ -41,11 +41,15 @@ export default function ProductList(props: Props) {
   const classes = useStyles();
   const [products, setProducts] = useState<Product[]>([]);
   const [previousCategoryId, setPreviousCategoryId] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
   const { categoryId: categoryIdParam } = useParams();
   const categoryId: number = isNaN(categoryIdParam) ? 0 : Number(categoryIdParam);
 
   useEffect(() => {
-    if (categoryId > 0 && previousCategoryId !== categoryId) {
+    if (!isLoading && categoryId > 0 && previousCategoryId !== categoryId) {
+      setIsLoading(true);
+      setError(null);
       client.listProductByCategory(categoryId)
         .then(response => response.json())
         .then(response => {
@@ -59,49 +63,75 @@ export default function ProductList(props: Props) {
               product.price,
             ));
             setProducts(newProducts);
-            setPreviousCategoryId(categoryId);
           }
+        })
+        .catch(error => {
+          setProducts([]);
+          setError(error)
+        })
+        .finally(() => {
+          setIsLoading(false);
+          setPreviousCategoryId(categoryId);
         })
     }
   });
 
+  const loadingContent = (
+    <Grid item xs={12}>
+      <CircularProgress />
+    </Grid>
+  );
+
+  const content = products.map(product => (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={product.photo !== '' ? product.photo : 'https://sabbaytinh.com/media/original/no-image.png'}
+            title={product.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {product.name}
+            </Typography>
+            <Typography variant="body2" color="textPrimary" component="p" gutterBottom>
+              {currencyFormatter.format(product.price)}
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="p">
+              {product.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Add to Cart
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  ));
+
+  const invalidCategoryContent = (
+    <Grid item xs={12}>
+      <Typography variant="body1">Select category first</Typography>
+    </Grid>
+  );
+
+  const errorContent = (
+    <Grid item xs={12}>
+      <Typography variant="body1" color="error">Sorry, something is broken</Typography>
+    </Grid>
+  );
+
   return (
     <Grid container spacing={3}>
       {
-        categoryId > 0 ?
-          products.map(product => (
-            <Grid item xs={12} sm={6} md={4}>
-              <Card className={classes.root}>
-                <CardActionArea>
-                  <CardMedia
-                    className={classes.media}
-                    image={product.photo !== '' ? product.photo : 'https://sabbaytinh.com/media/original/no-image.png'}
-                    title={product.name}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {product.name}
-                    </Typography>
-                    <Typography variant="body2" color="textPrimary" component="p" gutterBottom>
-                      {currencyFormatter.format(product.price)}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" component="p">
-                      {product.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    Add to Cart
-                </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          )) : (
-            <Grid item xs={12}>
-              <Typography variant="body1">Select category first</Typography>
-            </Grid>
+        isLoading ? loadingContent : (
+          error !== null ? errorContent : (
+            categoryId > 0 ? content : invalidCategoryContent
           )
+        )
       }
     </Grid>
   );
